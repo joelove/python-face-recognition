d65841262ae69dee07b42843c00277c38837bf27
Joe Love <git@joelove.uk>
2019-02-12 17:45:30 +0000

    add main name check to entry point

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import classes.webcam
 
-webcam = classes.webcam.Webcam()
-webcam.begin_capture()
+def main():
+    webcam = classes.webcam.Webcam()
+    webcam.begin_capture()
+
+if __name__ == '__main__':
+    main()
