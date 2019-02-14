97230ec6de0c597e767987845bdde0a9343585e5
Joe Love <git@joelove.uk>
2019-02-14 15:19:49 +0000

    clean up map using nested functions

--- utilities/face_utility.py
+++ utilities/face_utility.py
@@ -14,17 +14,20 @@ face_detector = dlib.get_frontal_face_detector()
 face_recognition = dlib.face_recognition_model_v1('./models/dlib_face_recognition_resnet_model_v1.dat')
 shape_predictor = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
 
+
 def face_to_vector(image, face):
     face_descriptor = face_recognition.compute_face_descriptor(image, face)
     face_vector = numpy.array(face_descriptor).astype(float)
 
     return face_vector
 
+
 def face_from_image(image, face):
     size = face.height() * face.width()
     shape = shape_predictor(image, face)
 
     return size, shape
+
 
 def faces_from_image(image):
     detected_faces = face_detector(image, 0)
@@ -33,22 +36,24 @@ def faces_from_image(image):
 
     return sorted_faces
 
-def identity_face(image, face, face_matrix, face_identifiers):
-    face_vector = face_to_vector(image, face)
-    differences = numpy.subtract(face_matrix, face_vector)
-    distances = numpy.linalg.norm(differences, axis=1)
-    closest_index = numpy.argmin(distances)
-    face_identifier = face_identifiers[closest_index]
-    distance = distances[closest_index]
-
-    return face_identifier, distance
 
 def identify_faces(image):
     analyzed_faces = numpy.load(FACES_FILE_PATH).item()
     face_identifiers = numpy.array(list(analyzed_faces.keys()))
     face_matrix = numpy.array(list(analyzed_faces.values()))
+
+    def identity_face(face):
+        face_vector = face_to_vector(image, face)
+        differences = numpy.subtract(face_matrix, face_vector)
+        distances = numpy.linalg.norm(differences, axis=1)
+        closest_index = numpy.argmin(distances)
+        face_identifier = face_identifiers[closest_index]
+        distance = distances[closest_index]
+
+        return face_identifier, distance
+
     faces_in_image = faces_from_image(image)
-    identified_faces = [identity_face(image, face, face_matrix, face_identifiers) for face in faces_in_image]
+    identified_faces = map(identity_face, faces_in_image)
 
     return identified_faces
 
